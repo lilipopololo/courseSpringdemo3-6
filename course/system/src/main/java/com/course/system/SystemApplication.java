954d9f368e97ccb1d640743fce5a68a54aa0f7d6
yjw <996543105@qq.com>
2021-03-07 17:18:37 +0800

    3-7-17

---
+++ course/system/src/main/java/com/course/system/SystemApplication.java
@@ -0,0 +1,21 @@
+package com.course.system;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+
+@SpringBootApplication
+public class EurekaApplication {
+	public static final Logger LOG = LoggerFactory.getLogger(EurekaApplication.class);
+	public static void main(String[] args) {
+//		SpringApplication.run(EurekaApplication.class, args);
+		SpringApplication app = new SpringApplication(EurekaApplication.class);
+		ConfigurableEnvironment environment = app.run(args).getEnvironment();
+		LOG.info("启动 Eureka");
+		LOG.info("System地址：http://127.0.0.1:{}", environment.getProperty("server.port"));
+	}
+
+}
---
+++ course/system/src/main/java/com/course/system/controller/TestController.java
@@ -0,0 +1,4 @@
+package com.course.system.controller;
+
+public class TestController {
+}
