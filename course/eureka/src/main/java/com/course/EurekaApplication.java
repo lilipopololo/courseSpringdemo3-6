5a610b8f670c2e9c3b16a9ca9fc4dbf2f42b5a86
yjw <996543105@qq.com>
2021-03-07 14:44:28 +0800

    3-7-13

--- course/eureka/src/main/java/com/course/EurekaApplication.java
+++ course/eureka/src/main/java/com/course/EurekaApplication.java
@@ -2,8 +2,10 @@ package com.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaApplication {
 	//将course的注册文件添加到Eureka子项目中
 	public static void main(String[] args) {
