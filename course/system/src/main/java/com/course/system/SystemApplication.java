9ade29ec64c730d8ce3d4500c7d38110a8877fd0
yjw <996543105@qq.com>
2021-03-07 17:19:00 +0800

    3-7-17

--- course/system/src/main/java/com/course/system/SystemApplication.java
+++ course/system/src/main/java/com/course/system/SystemApplication.java
@@ -8,11 +8,11 @@ import org.springframework.core.env.ConfigurableEnvironment;
 
 
 @SpringBootApplication
-public class EurekaApplication {
-	public static final Logger LOG = LoggerFactory.getLogger(EurekaApplication.class);
+public class SystemApplication {
+	public static final Logger LOG = LoggerFactory.getLogger(SystemApplication.class);
 	public static void main(String[] args) {
 //		SpringApplication.run(EurekaApplication.class, args);
-		SpringApplication app = new SpringApplication(EurekaApplication.class);
+		SpringApplication app = new SpringApplication(SystemApplication.class);
 		ConfigurableEnvironment environment = app.run(args).getEnvironment();
 		LOG.info("启动 Eureka");
 		LOG.info("System地址：http://127.0.0.1:{}", environment.getProperty("server.port"));
