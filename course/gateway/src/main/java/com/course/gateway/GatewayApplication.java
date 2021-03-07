2d02fe0ef138c844a743edd4642df06c5c70fa27
yjw <996543105@qq.com>
2021-03-07 18:34:08 +0800

    增加gateway子模块

---
+++ course/gateway/src/main/java/com/course/gateway/GatewayApplication.java
@@ -0,0 +1,22 @@
+package com.course.system;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class SystemApplication {
+	public static final Logger LOG = LoggerFactory.getLogger(SystemApplication.class);
+	public static void main(String[] args) {
+//		SpringApplication.run(EurekaApplication.class, args);
+		SpringApplication app = new SpringApplication(SystemApplication.class);
+		ConfigurableEnvironment environment = app.run(args).getEnvironment();
+		LOG.info("启动 Eureka");
+		LOG.info("System地址：http://127.0.0.1:{}", environment.getProperty("server.port"));
+	}
+
+}
